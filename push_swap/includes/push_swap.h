73b06a5047fbafa60b5e49bc7081297ec732c9b6
Lucas Henrique Bordonal <lbordona@c1r8s3.42porto.com>
2023-01-05 16:16:37 +0000

    05/01/23 - 16:15

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 17:22:39 by lbordona          #+#    #+#             */
-/*   Updated: 2023/01/04 17:24:42 by lbordona         ###   ########.fr       */
+/*   Updated: 2023/01/05 16:15:30 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
+
+void	sa(t_list *stack_a, t_list *stack_b);
 
 #endif
--- push_swap/srcs/swap.c
+++ push_swap/srcs/swap.c
@@ -1,44 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/04 17:22:50 by lbordona          #+#    #+#             */
-/*   Updated: 2023/01/05 16:15:27 by lbordona         ###   ########.fr       */
+/*   Created: 2023/01/05 16:05:50 by lbordona          #+#    #+#             */
+/*   Updated: 2023/01/05 16:15:51 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	print_list(t_list *list)
+void swap_a(t_list *stack_a, t_list *stack_b)
 {
-	while (list)
+	int size_a;
+	int size_b;
+
+	size_a = ft_lstsize(stack_a);
+	size_b = ft_lstsize(stack_b);
+
+	if (size_b == 0)
 	{
-		ft_putstr_fd(list->content, 1);
-		list = list->next;
+
 	}
+
+	return ;
 }
-
-int	main(int argc, char **argv)
-{
-	int	i;
-	t_list	*stack_a;
-	t_list	*new;
-
-	(void)argc;
-	i = 2;
-	stack_a = ft_lstnew(argv[1]);
-	while (argv[i])
-	{
-		new = ft_lstnew(argv[i]);
-		ft_lstadd_back(&stack_a, new);
-		i++;
-	}
-	print_list(stack_a);
-}
-
-
-
-
