f9b2821c8d1f479740cdf83def3cc9ad2e8e3cd1
Lucas Henrique Bordonal <lbordona@c1r10s1.42porto.com>
2023-01-20 11:27:27 +0000

    20/01/23 - 11:27

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 17:22:50 by lbordona          #+#    #+#             */
-/*   Updated: 2023/01/19 17:07:21 by lbordona         ###   ########.fr       */
+/*   Updated: 2023/01/20 11:02:50 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,10 +68,10 @@ int	main(int ac, char **av)
 	ft_printf("%s\n", "[double_rotate(stack_a)]");
 	ft_printstack(&stack_a); */
 
-	/* rra(&stack_a);
+	rra(&stack_a);
 	ft_printf("\n%s\n", "[rerotate_a(stack_a)]");
 	ft_printstack(&stack_a);
 	rrr(&stack_a, &stack_a);
 	ft_printf("%s\n", "[double_rerotate(stack_a)]");
-	ft_printstack(&stack_a); */
+	ft_printstack(&stack_a);
 }
